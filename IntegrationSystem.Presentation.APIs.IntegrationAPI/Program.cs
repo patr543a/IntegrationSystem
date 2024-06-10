d72683c491e85d4710259a1cdc2923f520e3d3e3
patr543a <113888937+patr543a@users.noreply.github.com>
2024-06-10 11:03:25 +0200

    Added comments

--- IntegrationSystem.Presentation.APIs.IntegrationAPI/Program.cs
+++ IntegrationSystem.Presentation.APIs.IntegrationAPI/Program.cs
@@ -1,19 +1,35 @@
+using IntegrationSystem.Application.Interfaces.UseCases;
+using IntegrationSystem.Application.UseCases;
+using IntegrationSystem.Infrastructure.Persistence.Contexts;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.UnitOfWorks;
+using IntegrationSystem.Infrastructure.Persistence.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+
+// Make WebApplication
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.AddServiceDefaults();
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DbContext, MergeUserContext>();
+builder.Services.AddTransient<IMergedUserRepository, MergedUserRepository>();
+builder.Services.AddTransient<IXmlUserRepository, XmlUserRepository>();
+builder.Services.AddTransient<IIntegrationSystemUnitOfWork, IntegrationSystemUnitOfWork>();
+builder.Services.AddTransient<IMergedUserUseCase, MergedUserUseCase>();
+builder.Services.AddKeyedSingleton("XmlFileLocation", "./TestData.xml");
+
+// Build the App
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
