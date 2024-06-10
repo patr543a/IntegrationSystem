4bfb8c1120f8f8af2dec5ae1c47e17452c9c2df6
patr543a <113888937+patr543a@users.noreply.github.com>
2024-06-10 11:02:16 +0200

    Added UnitOfWork interfaces

---
+++ IntegrationSystem.Infrastructure.Persistence.Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿namespace IntegrationSystem.Infrastructure.Persistence.Interfaces;
+
+/// <summary>
+/// Unit of work interface to be implemented by all unit of works.
+/// </summary>
+public interface IUnitOfWork
+{
+	/// <summary>
+	/// Commits all changes made.
+	/// </summary>
+	/// <returns>The number of entities/rows affected</returns>
+	Task<int> CommitAsync(CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Rollback all changes made.
+	/// </summary>
+	/// <returns>Awaitable Task</returns>
+	Task RollbackAsync(CancellationToken cancellationToken = default);
+}
---
+++ IntegrationSystem.Infrastructure.Persistence.Interfaces/UnitOfWorks/IIntegrationSystemUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using IntegrationSystem.Infrastructure.Persistence.Interfaces.Repositories;
+
+namespace IntegrationSystem.Infrastructure.Persistence.Interfaces.UnitOfWorks;
+
+/// <summary>
+/// The interface for a unit of work for the IntegrationSystem.
+/// </summary>
+public interface IIntegrationSystemUnitOfWork
+	: IUnitOfWork
+{
+	/// <summary>
+	/// The repository for the <see cref="MergedUser"/> entity.
+	/// </summary>
+	IMergedUserRepository MergedUserRepository { get; }
+
+	/// <summary>
+	/// The repository for the <see cref="XmlUser"/> entity.
+	/// </summary>
+	IXmlUserRepository XmlUserRepository { get; }
+}
