725f42ff53c651e7f5a69c8d89a704ff4deb2130
patr543a <113888937+patr543a@users.noreply.github.com>
2024-06-10 11:04:35 +0200

    Added comments

--- IntegrationSystem.Presentation.InternalServices.UserMerger/Program.cs
+++ IntegrationSystem.Presentation.InternalServices.UserMerger/Program.cs
@@ -1,10 +1,33 @@
+using IntegrationSystem.Application.Interfaces.UseCases;
+using IntegrationSystem.Application.UseCases;
+using IntegrationSystem.Infrastructure.Persistence.Contexts;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.UnitOfWorks;
+using IntegrationSystem.Infrastructure.Persistence.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.UnitOfWorks;
+using IntegrationSystem.Presentation.InternalServices.UserMerger.Services;
+using Microsoft.EntityFrameworkCore;
+
+// Make UserMerger service
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.AddServiceDefaults();
+
+builder.Services.AddDbContext<DbContext, MergeUserContext>();
+builder.Services.AddTransient<IMergedUserRepository, MergedUserRepository>();
+builder.Services.AddTransient<IXmlUserRepository, XmlUserRepository>();
+builder.Services.AddTransient<IIntegrationSystemUnitOfWork, IntegrationSystemUnitOfWork>();
+builder.Services.AddTransient<IMergedUserUseCase, MergedUserUseCase>();
+builder.Services.AddHostedService<BackgroundUserMergerService>();
+builder.Services.AddKeyedSingleton("XmlFileLocation", "./TestData.xml");
+builder.Services.AddKeyedSingleton("Azure_gRPC_Address", "http://localhost:5041");
+builder.Services.AddKeyedSingleton("XML_gRPC_Address", "http://localhost:5209");
+
+// Build the app
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 app.MapDefaultEndpoints();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- IntegrationSystem.Presentation.InternalServices.XML_GRPC/Program.cs
+++ IntegrationSystem.Presentation.InternalServices.XML_GRPC/Program.cs
@@ -1,17 +1,31 @@
+using IntegrationSystem.Infrastructure.Persistence.Contexts;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.Interfaces.UnitOfWorks;
+using IntegrationSystem.Infrastructure.Persistence.Repositories;
+using IntegrationSystem.Infrastructure.Persistence.UnitOfWorks;
 using IntegrationSystem.Presentation.InternalServices.XML_GRPC.Services;
+using Microsoft.EntityFrameworkCore;
 
+// Make gRPC service
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.AddServiceDefaults();
 
-// Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddDbContext<DbContext, MergeUserContext>();
+builder.Services.AddTransient<IMergedUserRepository, MergedUserRepository>();
+builder.Services.AddKeyedSingleton("XmlFileLocation", "./TestData.xml");
+builder.Services.AddTransient<IXmlUserRepository, XmlUserRepository>();
+builder.Services.AddTransient<IIntegrationSystemUnitOfWork, IntegrationSystemUnitOfWork>();
+
+// Build the app
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<XmlService>();
 
 app.Run();
